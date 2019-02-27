4edfc44a8e4cadf9dc93840ff7b42d273f74e6d0
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-02-27 15:39:46 +0100

    Fixed a bug (fetch link of repos)

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,7 +51,7 @@ function github_api() {
 
     // REPOSITORIES
 
-    fetch("https://api.github.com/users/peter-stuhlmann/repos?client_id=25bb194b081525d08147&client_secret=85f00c5312adc3596dbbc4c15ae7db009e99f9e5")
+    fetch(`https://api.github.com/users/${inputUserName}/repos?client_id=25bb194b081525d08147&client_secret=85f00c5312adc3596dbbc4c15ae7db009e99f9e5`)
         .then(
             response => response.json()
         )
