d65d734cbbe2e47b513adab05290a383a6881a3f
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-02-27 10:08:06 +0100

    Added API code for user and repositories and selected some user keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
 "use strict";
 
+// USER
+
+fetch("https://api.github.com/users/peter-stuhlmann?client_id=25bb194b081525d08147&client_secret=85f00c5312adc3596dbbc4c15ae7db009e99f9e5")
+    .then(
+        response => response.json()
+    )
+    .then(
+        user => {
+            let userInfos = [];
+            userInfos.push(`
+                <img id="gh_avatar" src="${user.avatar_url}">  
+                <div id="gh_username">${user.name}</div>
+                <div id="gh_login">${user.login}</div>
+                <div id="gh_bio">${user.bio}</div>
+                <div id="gh_location">${user.location}</div>
+                <div id="gh_email">${user.email}</div>
+                <div id="gh_blog">${user.blog}</div>
+            `)
+
+            document.querySelector('aside').innerHTML = userInfos.join('')
+        },
+    )
+    .catch(
+        err => console.log(`panic: ${err}`)
+    )
+
+
+
+
+
+
+
+
+
+// REPOSITORIES
+
 fetch("https://api.github.com/users/peter-stuhlmann/repos?client_id=25bb194b081525d08147&client_secret=85f00c5312adc3596dbbc4c15ae7db009e99f9e5")
     .then(
         response => response.json()
@@ -10,13 +46,17 @@ fetch("https://api.github.com/users/peter-stuhlmann/repos?client_id=25bb194b0815
             repos.forEach(
                 repo => {
                     repoList.push(`
-                        <!-- Content -->
+                        <li><a class="name" title="Redirect to github.com" target="_blanc" href="${repo.html_url}">${repo.name}</a>
+                            <ul>
+                                <li>${repo.description}</li>
+                            </ul>
+                        </li>                    
                     `)
                 }
             )
 
-            document.querySelector('.repositories').innerHTML = repoList.join('')
-        }
+            document.querySelector('.gh_repositories').innerHTML = repoList.join('')
+        },
     )
     .catch(
         err => console.log(`panic: ${err}`)
