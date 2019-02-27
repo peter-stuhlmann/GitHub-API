f13dfb6231d7ff7d889f48fd6b1b15d2f4966b93
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-02-27 10:43:49 +0100

    Added repository keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,9 +14,9 @@ fetch("https://api.github.com/users/peter-stuhlmann?client_id=25bb194b081525d081
                 <div id="gh_username">${user.name}</div>
                 <div id="gh_login">${user.login}</div>
                 <div id="gh_bio">${user.bio}</div>
-                <div id="gh_location">${user.location}</div>
-                <div id="gh_email">${user.email}</div>
-                <div id="gh_blog">${user.blog}</div>
+                <div id="gh_location"><i class="fas fa-map-marker-alt">&nbsp;</i>${user.location}</div>
+                <div id="gh_email"><i class="fas fa-envelope">&nbsp;</i><a href="mailto:${user.email}">${user.email}</a></div>
+                <div id="gh_blog"><i class="fas fa-link">&nbsp;</i><a href="${user.blog}">${user.blog}</a></div>
             `)
 
             document.querySelector('aside').innerHTML = userInfos.join('')
@@ -25,12 +25,6 @@ fetch("https://api.github.com/users/peter-stuhlmann?client_id=25bb194b081525d081
     .catch(
         err => console.log(`panic: ${err}`)
     )
-
-
-
-
-
-
 
 
 
@@ -47,9 +41,10 @@ fetch("https://api.github.com/users/peter-stuhlmann/repos?client_id=25bb194b0815
                 repo => {
                     repoList.push(`
                         <li><a class="name" title="Redirect to github.com" target="_blanc" href="${repo.html_url}">${repo.name}</a>
-                            <ul>
-                                <li>${repo.description}</li>
-                            </ul>
+                            <div>${repo.description}</div>
+                            <div><i class="fas fa-circle">&nbsp;</i>${repo.language}</div>
+                            <div><i class="fas fa-star">&nbsp;</i>${repo.stargazers_count}</div>
+                            <div><i class="fas fa-code-branch">&nbsp;</i>${repo.forks}</div>
                         </li>                    
                     `)
                 }
